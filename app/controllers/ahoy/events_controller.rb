89f1c860a92c5f309db82ccf4dff94fca1bf8856
Andrew Kane <acekane1@gmail.com>
2014-05-10 20:56:03 -0700

    Use time option

--- app/controllers/ahoy/events_controller.rb
+++ app/controllers/ahoy/events_controller.rb
@@ -2,7 +2,11 @@ module Ahoy
   class EventsController < Ahoy::BaseController
 
     def create
-      ahoy.track params[:name], params[:properties]
+      options = {}
+      if params[:time] and (time = Time.at(params[:time].to_f) rescue nil) and (1.minute.ago..Time.now).cover?(time)
+        options[:time] = time
+      end
+      ahoy.track params[:name], params[:properties], options
       render json: {}
     end
 
